e04e074acc006bed6e071cdcb513b3d1a35d8e78
marce <marc@gmail.com>
2026-03-16 11:18:05 -0400

    programa sin comentarios

--- programa_par.java
+++ programa_par.java
@@ -2,20 +2,14 @@ import java.util.Scanner;
 
 public class Prograama_par {
     public static void main(String[] args) {
-        // Crear un objeto Scanner para leer la entrada del usuario
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Ingrese un número: ");
-        
-        // Leer el número ingresado
+        System.out.print("intro. número: ");
         int numero = scanner.nextInt();
-        
-        // Verificar si el número es par
         if (numero % 2 == 0) {
-            System.out.println("El número " + numero + " es par.");
+            System.out.println("es par.");
         } else {
-            System.out.println("El número " + numero + " es impar.");
+            System.out.println("es impar.");
         }
-        
         scanner.close();
     }
 }
